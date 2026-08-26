05477fc0fbc77b0e773159314c55cc207fddea73
Ignacio Bockl <ignaciobockl96@gmail.com>
2026-08-26 14:49:55 -0300

    feat(core): export input modules
    - Barrel export for MouseHandler, TouchHandler, KeyboardHandler, WheelHandler, InputManager
    - Clean imports from '@pageflip/core/input'

---
+++ packages/core/src/input/index.ts
@@ -0,0 +1,9 @@
+/**
+ * Input Module Exports
+ * @packageDocumentation
+ */
+export * from "./MouseHandler";
+export * from "./TouchHandler";
+export * from "./KeyboardHandler";
+export * from "./WheelHandler";
+export * from "./InputManager";
