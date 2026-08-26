e929840f59a426ec754dc9f13e91f8dd7df1748c
Ignacio Bockl <ignaciobockl96@gmail.com>
2026-08-26 16:42:18 -0300

    feat(react): add pageflip hooks

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,1 +1,13 @@
-export {};
+export { PageFlip } from "./components/PageFlip";
+export type { PageFlipProps } from "./components/PageFlip";
+export { usePageFlip } from "./hooks/usePageFlip";
+export type {
+	UsePageFlipOptions,
+	UsePageFlipReturn,
+} from "./hooks/usePageFlip";
+export { usePageFlipControls } from "./hooks/usePageFlipControls";
+export type { PageFlipControls } from "./hooks/usePageFlipControls";
+export { usePageFlipState } from "./hooks/usePageFlipState";
+export type { PageFlipState } from "./hooks/usePageFlipState";
+export { usePageFlipEvents } from "./hooks/usePageFlipEvents";
+export type { PageFlipEventHandlers } from "./hooks/usePageFlipEvents";
