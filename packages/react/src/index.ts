c4736734198bc95ab8859e00056b8e7e5603dddb
Ignacio Bockl <ignaciobockl96@gmail.com>
2026-08-26 16:43:54 -0300

    feat(react): export all public APIs
    - PageFlip component
    - usePageFlip, usePageFlipControls, usePageFlipState, usePageFlipEvents hooks
    - All TypeScript types
    - Clean imports from '@pageflip/react'

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,13 +1,21 @@
+/**
+ * @pageflip/react - React 18+ Wrapper
+ * @packageDocumentation
+ */
 export { PageFlip } from "./components/PageFlip";
 export type { PageFlipProps } from "./components/PageFlip";
+
 export { usePageFlip } from "./hooks/usePageFlip";
 export type {
-	UsePageFlipOptions,
 	UsePageFlipReturn,
+	UsePageFlipOptions,
 } from "./hooks/usePageFlip";
+
 export { usePageFlipControls } from "./hooks/usePageFlipControls";
 export type { PageFlipControls } from "./hooks/usePageFlipControls";
+
 export { usePageFlipState } from "./hooks/usePageFlipState";
 export type { PageFlipState } from "./hooks/usePageFlipState";
+
 export { usePageFlipEvents } from "./hooks/usePageFlipEvents";
 export type { PageFlipEventHandlers } from "./hooks/usePageFlipEvents";
